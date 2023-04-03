a6971314986af062fe579445d4d484ee1bfad70d
alescrocaro <Alexandre A. Scrocaro Junior>
2023-04-02 21:27:15 -0300

    Add: CONNECT command server side

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
 import sys
 import socket
 import selectors
+import time
 import types
-
+import json
 
 # used to manage I/O operations on more than one socket
 selector = selectors.DefaultSelector()
 
 HOST = "127.0.0.1" # localhost
-PORT = 10667
+# PORT = 4444
+PORT = 7777
 
 # socket.AF_INET = internet address family for ipv4
 # socket.SOCK_STREAM = socket type for TCP
@@ -33,28 +35,89 @@ def service_connection(key, mask):
   sock = key.fileobj
   data = key.data
   if mask & selectors.EVENT_READ:
+    print('LEITURAAAAAAAAAAAAA')
+    print()
     recv_data = sock.recv(1024)  # Should be ready to read
+
     if recv_data:
       data.outb += recv_data
-      print(data)
+      print(f'data {data}')
+      print(f'recv_data {recv_data}')
+      print()
+
+      if recv_data == b"EXIT":
+        print(f"Closing connection to {data.addr}")
+        print()
+        selector.unregister(sock)
+        sock.close()
+        return
+      
+      command = recv_data.split(b" ")
+      print(command)
+
+      if command[0] == b"CONNECT":
+        try:
+          (username, password) = command[1].split(b",")
+
+          decodedUsername = username.decode("utf-8")
+          decodedPassword = password.decode("utf-8")
+
+          with open('users.json') as file:
+            users = json.load(file)['users']
+
+          for user in users:
+            print(user)
+            if user['username'] == decodedUsername and user['password'] == decodedPassword:
+              print('User authenticated.')
+              data.outb = b"SUCCESS"
+              break
+          else: # this else points to 'for' loop
+            print('Invalid username or password.')
+            data.outb = b"ERROR"
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+
+          print(users)
+
+          print(decodedUsername, decodedPassword)
+          print()
+
+
+        except:
+          data.outb = b"ERROR"
+          sent = sock.send(data.outb)  # Should be ready to write
+          data.outb = data.outb[sent:]
+
     else:
       print(data)
       print(f"Closing connection to {data.addr}")
+      print()
       selector.unregister(sock)
       sock.close()
   if mask & selectors.EVENT_WRITE:
+    print('ESCRITAAAAAAAAAAAAAA')
     if data.outb:
       print(f"Echoing {data.outb!r} to {data.addr}")
+      print()
       sent = sock.send(data.outb)  # Should be ready to write
       data.outb = data.outb[sent:]
 
 try:
   while True:
     events = selector.select(timeout=None)
+    print(f'events {events}')
     for key, mask in events:
+      print('ENTROU FOR')
       if key.data is None:
+        print(f' accept_wrapper key {key}')
+        print()
+        time.sleep(2)
         accept_wrapper(key.fileobj)
       else:
+        print(f'service_connection key {key}')
+        print(f'service_connection mask {mask}')
+        print()
+        time.sleep(2)
         service_connection(key, mask)
 except KeyboardInterrupt:
   print("Caught keyboard interrupt, exiting")
