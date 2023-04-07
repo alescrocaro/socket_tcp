715847f26fb977a947b68663ae9176cc05d5408e
alescrocaro <Alexandre A. Scrocaro Junior>
2023-04-07 14:43:52 -0300

    rename folders

--- questao2/teste.py
+++
@@ -1,6 +0,0 @@
-output_file = 'output.md'
-# Write numbers from 1 to 3000 to the output file
-
-with open(output_file, 'w') as f_out:
-  for number in range(1, 3001):
-    f_out.write(str(number) + '\n')
